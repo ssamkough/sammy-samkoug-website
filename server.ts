0e15bd58c31c1eac271dbeb473814f7cf08c07b0
Sammy <sammysamkough@hotmail.com>
2023-09-14 22:16:04 -0300

    multiple pages and organization

--- server.ts
+++ server.ts
@@ -4,8 +4,27 @@ const CSS_FILE_TYPE = ".css";
 
 const FILES = {
   "/": "index",
+  "/about": "about",
   "/styles.css": "styles",
 };
+
+const START_OF_FILE = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>sammy samkough</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="icon" type="image/x-icon" href="./assets/favicon.ico" />
+<link rel="stylesheet" type="text/css" href="./styles.css" />
+</head>
+<body>
+`;
+
+const END_OF_FILE = `
+</body>
+</html>
+`;
 
 const server = Deno.listen({ port: 8080 });
 console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
@@ -20,36 +39,34 @@ async function serve(conn: Deno.Conn) {
   for await (const requestEvent of httpConn) {
     const requestUrl = requestEvent.request.url;
     const path = new URL(requestUrl).pathname;
-    console.log("path", path);
+    requestEvent.respondWith(await response(path));
+  }
+}
 
-    switch (path) {
-      case "/": {
-        const file = await Deno.readFile(
-          `${DIRECTORY}${FILES[path]}${HTML_FILE_TYPE}`
-        );
-        const response = new Response(file, {
-          headers: { "content-type": "text/html; charset=utf-8" },
-          status: 200,
-        });
-        requestEvent.respondWith(response);
-        break;
-      }
-      case "/styles.css": {
-        const file = await Deno.readFile(
-          `${DIRECTORY}${FILES[path]}${CSS_FILE_TYPE}`
-        );
-        const response = new Response(file, {
-          headers: {
-            "content-type": "text/css",
-          },
-        });
-        requestEvent.respondWith(response);
-        break;
-      }
-      default: {
-        const response = new Response("404");
-        requestEvent.respondWith(response);
-      }
+async function response(path: string) {
+  switch (path) {
+    case "/":
+    case "/about": {
+      const file = await Deno.readTextFile(
+        `${DIRECTORY}${FILES[path]}${HTML_FILE_TYPE}`
+      );
+      return new Response(`${START_OF_FILE}${file}${END_OF_FILE}`, {
+        headers: { "content-type": "text/html; charset=utf-8" },
+        status: 200,
+      });
+    }
+    case "/styles.css": {
+      const file = await Deno.readFile(
+        `${DIRECTORY}${FILES[path]}${CSS_FILE_TYPE}`
+      );
+      return new Response(file, {
+        headers: {
+          "content-type": "text/css",
+        },
+      });
+    }
+    default: {
+      return new Response("404");
     }
   }
 }
