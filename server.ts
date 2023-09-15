32287088e3a89453566c74c791a6f00d7c482bc8
Sammy <sammysamkough@hotmail.com>
2023-09-14 22:22:12 -0300

    get favicon to work

--- server.ts
+++ server.ts
@@ -1,13 +1,15 @@
-const DIRECTORY = "public/";
+const SRC_DIRECTORY = "public/";
+const ASSETS_DIRECTORY = "assets/";
 const HTML_FILE_TYPE = ".html";
 const CSS_FILE_TYPE = ".css";
-
+const ICO_FILE_TYPE = ".ico";
 const FILES = {
   "/": "index",
   "/about": "about",
   "/styles.css": "styles",
+  "/favicon.ico": "favicon",
+  "/assets/favicon.ico": "favicon",
 };
-
 const START_OF_FILE = `
 <!DOCTYPE html>
 <html lang="en">
@@ -20,7 +22,6 @@ const START_OF_FILE = `
 </head>
 <body>
 `;
-
 const END_OF_FILE = `
 </body>
 </html>
@@ -39,6 +40,7 @@ async function serve(conn: Deno.Conn) {
   for await (const requestEvent of httpConn) {
     const requestUrl = requestEvent.request.url;
     const path = new URL(requestUrl).pathname;
+    console.log("path", path);
     requestEvent.respondWith(await response(path));
   }
 }
@@ -48,7 +50,7 @@ async function response(path: string) {
     case "/":
     case "/about": {
       const file = await Deno.readTextFile(
-        `${DIRECTORY}${FILES[path]}${HTML_FILE_TYPE}`
+        `${SRC_DIRECTORY}${FILES[path]}${HTML_FILE_TYPE}`
       );
       return new Response(`${START_OF_FILE}${file}${END_OF_FILE}`, {
         headers: { "content-type": "text/html; charset=utf-8" },
@@ -57,13 +59,20 @@ async function response(path: string) {
     }
     case "/styles.css": {
       const file = await Deno.readFile(
-        `${DIRECTORY}${FILES[path]}${CSS_FILE_TYPE}`
+        `${SRC_DIRECTORY}${FILES[path]}${CSS_FILE_TYPE}`
       );
       return new Response(file, {
         headers: {
           "content-type": "text/css",
         },
       });
+    }
+    case "/favicon.ico":
+    case "/assets/favicon.ico": {
+      const file = await Deno.readFile(
+        `${ASSETS_DIRECTORY}${FILES[path]}${ICO_FILE_TYPE}`
+      );
+      return new Response(file);
     }
     default: {
       return new Response("404");
