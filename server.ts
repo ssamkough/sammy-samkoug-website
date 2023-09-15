087549599b123b461d9b9efe434d7187eca5ec09
Sammy <sammysamkough@hotmail.com>
2023-09-14 22:35:55 -0300

    get 404 to work

--- server.ts
+++ server.ts
@@ -10,22 +10,6 @@ const FILES = {
   "/favicon.ico": "favicon",
   "/assets/favicon.ico": "favicon",
 };
-const START_OF_FILE = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>sammy samkough</title>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<link rel="icon" type="image/x-icon" href="./assets/favicon.ico" />
-<link rel="stylesheet" type="text/css" href="./styles.css" />
-</head>
-<body>
-`;
-const END_OF_FILE = `
-</body>
-</html>
-`;
 
 const server = Deno.listen({ port: 8080 });
 console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
@@ -36,11 +20,9 @@ for await (const conn of server) {
 
 async function serve(conn: Deno.Conn) {
   const httpConn = Deno.serveHttp(conn);
-
   for await (const requestEvent of httpConn) {
     const requestUrl = requestEvent.request.url;
     const path = new URL(requestUrl).pathname;
-    console.log("path", path);
     requestEvent.respondWith(await response(path));
   }
 }
@@ -48,34 +30,64 @@ async function serve(conn: Deno.Conn) {
 async function response(path: string) {
   switch (path) {
     case "/":
-    case "/about": {
-      const file = await Deno.readTextFile(
-        `${SRC_DIRECTORY}${FILES[path]}${HTML_FILE_TYPE}`
-      );
-      return new Response(`${START_OF_FILE}${file}${END_OF_FILE}`, {
-        headers: { "content-type": "text/html; charset=utf-8" },
-        status: 200,
-      });
-    }
+    case "/about":
+      return await page(path);
     case "/styles.css": {
-      const file = await Deno.readFile(
-        `${SRC_DIRECTORY}${FILES[path]}${CSS_FILE_TYPE}`
-      );
-      return new Response(file, {
-        headers: {
-          "content-type": "text/css",
-        },
-      });
+      return await stylesheet(path);
     }
     case "/favicon.ico":
     case "/assets/favicon.ico": {
-      const file = await Deno.readFile(
-        `${ASSETS_DIRECTORY}${FILES[path]}${ICO_FILE_TYPE}`
-      );
-      return new Response(file);
+      return await icon(path);
     }
     default: {
-      return new Response("404");
+      return await page(path);
     }
   }
+}
+
+async function page(path: "/" | "/about" | string) {
+  let page;
+  switch (path) {
+    case "/":
+    case "/about": {
+      page = await Deno.readTextFile(
+        `${SRC_DIRECTORY}/pages/${FILES[path]}${HTML_FILE_TYPE}`
+      );
+      break;
+    }
+    default: {
+      page = await Deno.readTextFile(
+        `${SRC_DIRECTORY}/pages/404${HTML_FILE_TYPE}`
+      );
+    }
+  }
+
+  const start = await Deno.readTextFile(
+    `${SRC_DIRECTORY}/components/start${HTML_FILE_TYPE}`
+  );
+  const end = await Deno.readTextFile(
+    `${SRC_DIRECTORY}/components/end${HTML_FILE_TYPE}`
+  );
+  return new Response(`${start}${page}${end}`, {
+    headers: { "content-type": "text/html; charset=utf-8" },
+    status: 200,
+  });
+}
+
+async function stylesheet(path: "/styles.css") {
+  const file = await Deno.readFile(
+    `${SRC_DIRECTORY}${FILES[path]}${CSS_FILE_TYPE}`
+  );
+  return new Response(file, {
+    headers: {
+      "content-type": "text/css",
+    },
+  });
+}
+
+async function icon(path: "/favicon.ico" | "/assets/favicon.ico") {
+  const file = await Deno.readFile(
+    `${ASSETS_DIRECTORY}${FILES[path]}${ICO_FILE_TYPE}`
+  );
+  return new Response(file);
 }
